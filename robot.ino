b1fdcd0e4cf587657c131002c685331d0c440cc6
Felipe Jiménez Ramírez <71561190+UFelipeJR@users.noreply.github.com>
2024-10-22 21:02:36 -0500

    update code

--- robot.ino
+++ robot.ino
@@ -1,75 +1,47 @@
-int sensor0 = A0;
-int sensor1 = A1;
-int sensor2 = A2;
-int sensor3 = A3;
-int sensor4 = A4;
-int sensor5 = A5;
-int sensor6 = A6;
-int sensor7 = A7;
-
+int sensors[8] = {A0, A1, A2, A3, A4, A5, A6, A7};
 int values[8];
-int SensorCount = 8;
-int minL = 750;
+bool sensorStates[8];
+int contInter=0;
+int Umbral = 750;
+unsigned long previousMillis = 0;  // Almacena el último tiempo en que se actualizó contInter
+const unsigned long interval = 2000;  // Intervalo de 2 segundos en milisegundos
+// Variable microsuiche
+//variable suiche
+
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(sensor0,INPUT);
-  pinMode(sensor1,INPUT);
-  pinMode(sensor2,INPUT);
-  pinMode(sensor3,INPUT);
-  pinMode(sensor4,INPUT);
-  pinMode(sensor5,INPUT);
-  pinMode(sensor6,INPUT);
-  pinMode(sensor7,INPUT);
   Serial.begin(9600);
-
 }
 
 void loop() {
   readsensors();
-  printValues();
-}
-
-void printValues()
-{
-
-  for (int i = 0; i < SensorCount; i++) {
-    Serial.print(values[i]);
-    Serial.print("   \t");
-  }
-  Serial.println();
-
-  delay(500);
-
-}
-
-void readsensors(){
-  
-  values[0] = analogRead(sensor0);
-  values[1] = analogRead(sensor1);
-  values[2] = analogRead(sensor2);
-  values[3] = analogRead(sensor3);
-  values[4] = analogRead(sensor4);
-  values[5] = analogRead(sensor5);
-  values[6] = analogRead(sensor6);
-  values[7] = analogRead(sensor7);
-  
   
 }
 
-void detectLine(){
-  for (int i = 0; i < SensorCount; i++) {
-    if(values[i] > minL){
-      Serial.print("En línea");
-      Serial.print(" \t");
-    }
-    else{
-      Serial.print("No línea");
-      Serial.print(" \t");
+void readsensors() {
+  bool allBlack = true;
+  for (int i = 0; i < 8; i++) {
+    values[i] = analogRead(sensors[i]);
+    sensorStates[i] = (values[i] > Umbral);
+
+   
+    Serial.print(sensorStates[i] ? 1 : 0);
+    Serial.print(" ");  
+    if (!sensorStates[i]) {
+      allBlack = false;
     }
   }
   Serial.println();
+  if (allBlack) {
+    unsigned long currentMillis = millis();  // Obtiene el tiempo actual en milisegundos
 
-  delay(500);
-  
+    // Si han pasado al menos 2 segundos desde la última vez que se incrementó contInter
+    if (currentMillis - previousMillis >= interval) {
+      contInter++;
+      previousMillis = currentMillis;  // Actualiza el tiempo del último incremento
+
+      Serial.print("Interseccion: ");
+      Serial.println(contInter);
+    }
+  }
 }
