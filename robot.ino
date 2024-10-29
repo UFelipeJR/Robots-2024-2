b0ce65ee54906dc0d37a97cbceadaa3f5915f3cd
Felipe Jiménez Ramírez <71561190+UFelipeJR@users.noreply.github.com>
2024-10-28 22:50:23 -0500

    update

--- robot.ino
+++ robot.ino
@@ -1,4 +1,5 @@
-int sensors[8] = {A0, A1, A2, A3, A4, A5, A6, A7};
+int sensors[8] = {A7, A6, A5, A4, A3, A2, A1, A0};
+
 int values[8];
 bool sensorStates[8];
 int contInter=0;
@@ -9,24 +10,34 @@ const unsigned long interval = 2000;  // Intervalo de 2 segundos en milisegundos
 //variable suiche
 
 
+int ain1 = 7;
+int ain2 = 6;
+int ena = 8;
+int bin1 = 0;
+int bin2 = 0;
+int enb = 0;
+
+int P = 0;
+
 void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
+void loop(){
   readsensors();
-  
+  calP();
 }
 
 void readsensors() {
   bool allBlack = true;
+  
   for (int i = 0; i < 8; i++) {
     values[i] = analogRead(sensors[i]);
     sensorStates[i] = (values[i] > Umbral);
 
    
-    Serial.print(sensorStates[i] ? 1 : 0);
-    Serial.print(" ");  
+    //Serial.print(sensorStates[i] ? 1 : 0);
+    //Serial.print(" ");  
     if (!sensorStates[i]) {
       allBlack = false;
     }
@@ -40,8 +51,75 @@ void readsensors() {
       contInter++;
       previousMillis = currentMillis;  // Actualiza el tiempo del último incremento
 
-      Serial.print("Interseccion: ");
-      Serial.println(contInter);
     }
   }
+}
+
+void move(int motor, int speed, int direction){
+
+  boolean inPin1 = LOW;
+  boolean inPin2 = HIGH;
+
+  if(direction == 1){
+    inPin1 = HIGH;
+    inPin2 = LOW;
+  }
+
+
+  if(motor == 1){
+    digitalWrite(ain1, inPin1);
+    digitalWrite(ain2, inPin2);
+    analogWrite(ena, speed);
+  }
+  else{
+    digitalWrite(bin1, inPin1);
+    digitalWrite(bin2, inPin2);
+    analogWrite(enb, speed);
+  }
+
+}
+
+void conditions(){
+  if(!sensorStates[0]&&!sensorStates[1]&&!sensorStates[2]&&sensorStates[3]&&sensorStates[4]&&!sensorStates[5]&&!sensorStates[6]&&!sensorStates[7]){
+      Serial.println("Adelante");
+  } 
+  else if(sensorStates[1] || sensorStates[2]){
+      Serial.println("Giro Izquierda");
+  }
+  else if(sensorStates[5] || sensorStates[6]){
+      Serial.println("Giro Derecha");
+  }
+  else if(sensorStates[0]){
+      Serial.println("Giro Izquierda Brusco");
+  }
+  else if(sensorStates[7]){
+      Serial.println("Giro Derecha Brusco");
+  }
+  delay(500);
+}
+
+void calP() {
+  
+  int positions[8] = {-3500, -2500, -1500, -500, 500, 1500, 2500, 3500};
+
+  int sum = 0; 
+  int count = 0;  
+
+  for (int i = 0; i < 8; i++) {
+    if (sensorStates[i]) {  
+      sum += positions[i];
+      count++;
+    }
+  }
+
+  
+  if (count > 0) {
+    P = sum / count;
+  } else {
+    P = 0; 
+  }
+
+  // Mostrar el valor de P para verificación
+  Serial.print("Posición P: ");
+  Serial.println(P);
 }
